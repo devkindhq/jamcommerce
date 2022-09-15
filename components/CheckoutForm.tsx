c6c35f6d29dc6e43f2d8f82a37318343ce162cc6
Ali Kazim <ali@devkind.com.au>
2022-09-16 04:36:06 +1000

    Adding product on the checkout page -- have to move this somewhere else

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -5,8 +5,10 @@ import StripeTestCards from '../components/StripeTestCards'
 
 import getStripe from '../utils/get-stripejs'
 import { fetchPostJSON } from '../utils/api-helpers'
-import { formatAmountForDisplay } from '../utils/stripe-helpers'
+import { formatAmountForDisplay, formatAmountForStripe } from '../utils/stripe-helpers'
 import * as config from '../config'
+import Products from './ProductsNew'
+import { Product } from 'use-shopping-cart'
 
 const CheckoutForm = () => {
   const [loading, setLoading] = useState(false)
@@ -40,6 +42,48 @@ const CheckoutForm = () => {
       // available to this file, so you can provide it as parameter here
       // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
       sessionId: response.id,
+
+      // TODO: Send user email if known
+      // TODO: Send user name if known
+      // TODO: Send line items if possible? giving levels are line items perhaps?
+    })
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message)
+    setLoading(false)
+  }
+
+  const getStripeProduct = (product: Product) => {
+    return {
+      name: product.name,
+      currency: product.currency,
+      description: product.description,
+      amount: product.price,
+      images: [product.image],
+      quantity: 1
+    }
+  }
+  const handleSingleProduct = async (product: Product) => {
+    setLoading(true)
+    // Create a Checkout Session.
+    const response = await fetchPostJSON('/api/checkout_sessions', {
+      amount: input.customDonation,
+      line_items: [getStripeProduct(product)]
+    })
+
+    if (response.statusCode === 500) {
+      console.error(response.message)
+      return
+    }
+
+    // Redirect to Checkout.
+    const stripe = await getStripe()
+    const { error } = await stripe!.redirectToCheckout({
+      // Make the id field from the Checkout Session creation API response
+      // available to this file, so you can provide it as parameter here
+      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+      sessionId: response.id,  
     })
     // If `redirectToCheckout` fails due to a browser or network
     // error, display the localized error message to your customer
@@ -49,7 +93,7 @@ const CheckoutForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <><form onSubmit={handleSubmit}>
       <CustomDonationInput
         className="checkout-style"
         name={'customDonation'}
@@ -68,7 +112,10 @@ const CheckoutForm = () => {
       >
         Donate {formatAmountForDisplay(input.customDonation, config.CURRENCY)}
       </button>
+
     </form>
+    <Products buyNow={true} handleBuyNow={handleSingleProduct} />
+    </>
   )
 }
 
