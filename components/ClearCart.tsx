b7ec085555d39ed1793e8dc4d767a9fc540143cf
Ali Kazim <ali@devkind.com.au>
2022-09-16 02:19:32 +1000

    Adding supabase and prisma db

--- components/ClearCart.tsx
+++ components/ClearCart.tsx
@@ -4,7 +4,7 @@ import { useShoppingCart } from 'use-shopping-cart/react'
 export default function ClearCart() {
   const { clearCart } = useShoppingCart()
 
-  useEffect(() => clearCart(), [clearCart])
+  useEffect(() => clearCart(), [])
 
   return <p>Cart cleared.</p>
 }
