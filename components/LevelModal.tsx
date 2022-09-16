76124b79a34cd6aa11f8b74b8b520a233698d732
Ali Kazim <ali@devkind.com.au>
2022-09-17 08:16:39 +1000

    Making changes on model level

--- components/LevelModal.tsx
+++ components/LevelModal.tsx
@@ -9,17 +9,19 @@ import {
     Button,
     Text,
 } from '@chakra-ui/react'
+import CheckoutForm from './CheckoutForm'
 
 
 export default function LevelModal({ isOpen, onClose }: { isOpen: boolean, onClose: () => void }) {
     return (
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={onClose} size="full">
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader>Modal Title</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
                     <Text>Sed porttitor lectus nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</Text>
+                    <CheckoutForm />
                 </ModalBody>
 
                 <ModalFooter>
