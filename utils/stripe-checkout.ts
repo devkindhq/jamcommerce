185f57d2e3d6fe07c80451c3aa27faf5b1366d89
Ali Kazim <ali@devkind.com.au>
2022-09-18 07:18:20 +1000

    adding custom amount checkout

--- utils/stripe-checkout.ts
+++ utils/stripe-checkout.ts
@@ -34,4 +34,28 @@ async function customCheckoutRedirect(product: Product, amount: number, currency
     console.warn(error.message)
   }
 
+export const customAmountCheckoutRedict = async (amount: number) => {
+      // Create a Checkout Session.
+      const response = await fetchPostJSON('/api/checkout_sessions', {
+        amount: amount,
+      })
+  
+      if (response.statusCode === 500) {
+        console.error(response.message)
+        return
+      }
+  
+      // Redirect to Checkout.
+      const stripe = await getStripe()
+      const { error } = await stripe!.redirectToCheckout({
+        // Make the id field from the Checkout Session creation API response
+        // available to this file, so you can provide it as parameter here
+        // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+        sessionId: response.id,
+      })
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `error.message`.
+      console.warn(error.message)
+}
 export default customCheckoutRedirect
