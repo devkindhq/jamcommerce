b91a3a725531af0a0ed21e9951289873bccc4b5e
Ali Kazim <ali@devkind.com.au>
2022-09-17 10:27:49 +1000

    Fixing campaign card

--- components/CampaignCard.tsx
+++ components/CampaignCard.tsx
@@ -12,10 +12,8 @@ import {
 import { BiBulb } from 'react-icons/bi';
 
 interface TestimonialCardProps {
-    name: string;
-    role: string;
+    title: string;
     content: string;
-    avatar: string;
     index: number;
   }
   
@@ -27,7 +25,7 @@ interface TestimonialCardProps {
       `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED8936'/%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%2348BB78'/%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%230BC5EA'/%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED64A6'/%3E%3C/svg%3E")`,
       `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ECC94B'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%239F7AEA'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%234299E1'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%2348BB78'/%3E%3C/svg%3E")`,
     ];
-    const { name, role, content, avatar, index } = props;
+    const { title, content, index } = props;
     return (
       <Flex
         boxShadow={'md'}
@@ -70,7 +68,7 @@ interface TestimonialCardProps {
             <Box>
             <HStack color={useColorModeValue('gray.800', 'white')}>
             <Icon as={BiBulb} fontSize={'2xl'}  />
-            <chakra.p fontWeight={'bold'} fontSize={'2xl'}> Campaign Objective</chakra.p>
+            <chakra.p fontWeight={'bold'} fontSize={'2xl'}> {title}</chakra.p>
               </HStack>
           <chakra.p
           my={2} fontSize={'2xl'}>
