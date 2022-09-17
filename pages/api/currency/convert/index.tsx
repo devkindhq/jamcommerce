232483c7efc22f6ade98a3870a6e66729bf0b79c
Ali Kazim <ali@devkind.com.au>
2022-09-18 04:05:01 +1000

    Fixing currency conversion

--- pages/api/currency/convert/index.tsx
+++ pages/api/currency/convert/index.tsx
@@ -5,22 +5,41 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
+// TODO: Move htis somewhere useful
+// NOTE: The amount here must be in 10s. e.g. 500 = 5 dollar. We want 500. This is how Stripe does it.
+export const convertRate = async (code: string, amount: number) => {
+  const currency_rate = await supabase.from("currency_rates").select('*').eq('code', code.toUpperCase()).order('updated_at', {ascending: false}).limit(1).single();
+  return {
+    ...currency_rate,
+    ...({data: Object.assign({},currency_rate.data, 
+      (amount && {amount:(currency_rate.data.value * (amount))})
+      )}),
+  }
+}
+
+// TODO: Move htis somewhere useful
+export const convertRateToBase = async (code: string, amount: number) => {
+  const currency_rate = await supabase.from("currency_rates").select('*').eq('code', code.toUpperCase()).order('updated_at', {ascending: false}).limit(1).single(),
+  currency_calculation = amount/currency_rate.data.value ;
+  return {
+    ...currency_rate,
+    ...({data: Object.assign({},currency_rate.data, 
+      (amount && {amount: currency_calculation})
+      )}),
+  }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method === 'POST') {
     try {
-      const { authorization } = req.headers;
-        const { code, amount  } = req.query;
-        let currency_rate = await supabase.from("currency_rates").select('*').eq('code', code).order('updated_at', {ascending: false}).limit(1).single();
-        const finalResult = {
-          ...currency_rate,
-          ...({data: Object.assign({},currency_rate.data, 
-            (amount && {amount:(currency_rate.data.value * amount)})
-            )}),
-        }
-        return res.status(200).json(finalResult);
+      const { authorization } = req.headers; // TODO: implement CORS here? 
+        const { code, amount } = req.query;
+        const result = await convertRate(code, amount);
+
+        return res.status(200).json(result);
     } catch (err) {
       res.status(500).json({ statusCode: 500, message: err.message });
     }
