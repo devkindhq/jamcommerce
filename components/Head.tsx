b9445a235a7b3a2dfbf4eb0c522766d73e28cc1b
Ali Kazim <ali@devkind.com.au>
2022-09-25 20:24:11 +1000

    Fixing share heads

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,25 +1,19 @@
-import {default as NextHeader} from 'next/head'
+import { useColorModeValue } from '@chakra-ui/react'
+import { default as NextHeader } from 'next/head'
+import { share_image, share_subject, share_url } from '../data/social_links'
+import OGHead from './OGHead'
 
 type HeaderProps = {
     title: string
 }
 export default function Head({title}:HeaderProps){
-    {/** TODO: Add writes description */}
+const ogDescription = "Pakistan is facing adverse climate change where the lives of more than 33 million people have been affected. Many have lost their home, cattle, crops, farms and also lives. Shahul & Zeenat Foundation is helping provide meals, shelter and medical aid to the affected in Sukkur and the nearby districts."
 return (    
     <NextHeader>
     <title>{title}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@thorwebdev" />
-    <meta name="twitter:title" content="TypeScript Next.js Stripe Example" />
-    <meta
-      name="twitter:description"
-      content="Full-stack TypeScript example using Next.js, react-stripe-js, and stripe-node."
-    />
-    <meta
-      name="twitter:image"
-      content="https://nextjs-typescript-react-stripe-js.vercel.app/social_card.png"
-    />
+    <meta name="theme-color" content={useColorModeValue("white","black")}></meta>
+    <OGHead title={share_subject} description={ogDescription} image={share_image} url={share_url} />
     </NextHeader>)
 }
