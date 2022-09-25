6e2dea309b45df1bc3eb1c08fb3e83861a245622
Ali Kazim <ali@devkind.com.au>
2022-09-25 16:21:29 +1000

    Adding currency in the layout to reflect from query

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,7 @@
 import { Box, useBreakpointValue } from '@chakra-ui/react'
-import { ReactNode } from 'react'
+import { useRouter } from 'next/router'
+import { ReactNode, useCallback, useContext, useEffect } from 'react'
+import AppContext from '../context/app-context'
 
 import Footer from './Footer'
 import Head from './Head'
@@ -13,16 +15,28 @@ type Props = {
 const Layout = ({
   children,
   title = 'Regional Sindh Emergency Flood Relief 2022',
-}: Props) => (
-  <>
+}: Props) => {
+  const { query } = useRouter();
+  const {currency, status} = query;
+  debugger;
+  const app = useContext(AppContext);
+  // This will read the currency from the params.
+  const changedCurrency  = useCallback(() => app.changeCurrency(currency as string), [query]);
+  useEffect(() => {
+    if(currency && app?.state?.currency_rates && app.state.currency_rates.length > 1){
+      changedCurrency()
+    }
+  }, [currency, app?.state?.currency_rates])
+
+  return(<>
     <Head title={title} />
-    <Header />
+    <Header status={status as string} />
     <Box pt={useBreakpointValue({base: '0px', md: '100px'})}>
       {children}
     </Box>
     <Footer />
-</>
- 
-)
+</>)
+}
+
 
 export default Layout
